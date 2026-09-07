df183a259b1698bacfa7f3b491944a55c41917b8
Matthias Daues <matthias.daues1@vodafone.com>
2026-09-07 16:51:59 +0200

    adhere to strict format: add three hyphens to the start of a yaml file

--- packages/factory/scripts/dispatch_lib.py
+++ packages/factory/scripts/dispatch_lib.py
@@ -453,8 +453,8 @@ class Ledger:
 
 def _dump_yaml(data: dict[str, Any]) -> str:
     if yaml is not None:
-        return yaml.dump(data, default_flow_style=False, sort_keys=False)
-    return _stdlib_dump(data)
+        return "---\n" + yaml.dump(data, default_flow_style=False, sort_keys=False)
+    return "---\n" + _stdlib_dump(data) + "\n"
 
 
 def _load_yaml(text: str) -> dict[str, Any]:
